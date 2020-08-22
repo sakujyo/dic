fd23d445474b75770b955c2d7e0bf88ad0001baa
s <tomo3u3u@gmail.com>
2020-08-23 01:09:47 +0900

    dic referencing by keyboard, modify style

--- dic.js
+++ dic.js
@@ -10,19 +10,37 @@ function tag() {
 		}
 	}
 	var s = '';
+	var i = 0;
 	for (var m of d.matchAll(/((?:<[^>]+>)*)([^<]*)/gi)) {
 		s += m[1];
 		s += m[2].replace(/[a-z]+/gi, (m, o, s) => {
-			return '<span class="w">' + m + '</span>' 
+			return '<span class="w" tabindex=' + i++ + '>' + m + '</span>' 
 		});
 	}
-	document.body.innerHTML = s + '<div id="dic"></div><style>\n.w {\n  color: #777;\n}\n#dic { position:fixed; display: block; width: 25%; height: 10%; left:75%; top: 90%; color: #fff; background-color: #232323; }</style>';
+	document.body.innerHTML = s + '<div id="dic"></div><style>\n.w {\n background-color: #fcfcfc;\n}\n#dic { position:fixed; display: block; width: 25%; height: 10%; right: 2%; bottom: 2%; color: #fff; background-color: #232323; padding: 5px; border-radius: 4px; box-shadow: 0 0 6px -1px #a3a3a3; z-index: 999999 }\n:focus { border-style: solid none solid; }\n</style>';
 
-	var d = { word: '単語', };
+	var d = { the: 'その', a: 'ひとつの', an: 'ひとつの' };
 	for (var el of document.getElementsByClassName('w')) {
-		el.addEventListener('click', (e) => {
+		el.addEventListener('keydown', (e) => {
+			if (e.key == "Tab") return;
 			console.log(e);
+			console.log(e.key);
+			console.log(e.target);
 			console.log(e.target.textContent);
+			//if ((e.key != 's') || (e.key != 'Enter')) return;
+			if (e.key == "a") {
+				let p = e.target.parentElement;
+				if (p.tagName == 'A') {
+					window.open(p.href);
+				}
+			} else if (e.key == 'q') {
+				let f = (e) => { if (e.tagName == 'BODY') { return 0; } else { console.log(e.offsetTop + ':' + e.tagName); return e.offsetTop + f(e.offsetParent); }}
+				let renderHeight = window.innerHeight;
+				let rh = renderHeight / 2;
+				let y = f(e.target) + e.target.offsetHeight / 2 - 1;
+				window.scrollTo(0, y - rh);
+			}
+			if ((e.key != 'w')) return;
 			let word = e.target.textContent;
 			if (word in d) {
 				document.getElementById('dic').textContent = d[word];
